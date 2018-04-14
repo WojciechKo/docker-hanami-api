58fc4ffc575032293cee15349668ba0c5664e9b5
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-04-14 21:54:15 +0100

    Setup logger level by LOGGER_LEVEL env

--- config/environment.rb
+++ config/environment.rb
@@ -8,42 +8,10 @@ Hanami.configure do
   mount Api::Application, at: '/'
 
   model do
-    ##
-    # Database adapter
-    #
-    # Available options:
-    #
-    #  * SQL adapter
-    #    adapter :sql, 'sqlite://db/bikeramp_development.sqlite3'
-    #    adapter :sql, 'postgresql://localhost/bikeramp_development'
-    #    adapter :sql, 'mysql://localhost/bikeramp_development'
-    #
     adapter :sql, ENV['DATABASE_URL']
-
-    ##
-    # Migrations
-    #
     migrations 'db/migrations'
     schema     'db/schema.sql'
   end
 
-  mailer do
-    root 'lib/bikeramp/mailers'
-
-    # See http://hanamirb.org/guides/mailers/delivery
-    delivery :test
-  end
-
-  environment :development do
-    # See: http://hanamirb.org/guides/projects/logging
-    logger level: :debug
-  end
-
-  environment :production do
-    logger level: :info, formatter: :json, filter: []
-
-    mailer do
-      delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
-    end
-  end
+  logger level: ENV.fetch('LOGGER_LEVEL')
 end
