7be048de24cb6faee72a6600960fc0eecbdd79f9
Wojciech Korzeniowski <korzeniowski.wojciech@gmail.com>
2018-04-15 14:36:40 +0100

    Return json

--- apps/api/controllers/stats/monthly.rb
+++ apps/api/controllers/stats/monthly.rb
@@ -2,7 +2,7 @@ class Api::Controllers::Stats::Monthly
   include Api::Action
 
   def call(params)
-    self.body = 'Hello there'
+    self.body = {message: 'Hello there'}.to_json
     self.status = 201
   end
 end
--- apps/api/controllers/trips/create.rb
+++ apps/api/controllers/trips/create.rb
@@ -2,7 +2,7 @@ class Api::Controllers::Trips::Create
   include Api::Action
 
   def call(params)
-    self.body = 'Hello there'
+    self.body = {message: 'Hello there'}.to_json
     self.status = 201
   end
 end
